1d897f891c9d3d976645c966d0d6b3d71f5224b0
凡隐 <337274802@qq.com>
2018-04-23 15:59:28 +0800

    logback

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # statemachine
 状态机学习
+
+https://github.com/oxo42/stateless4j
+
+http://projects.spring.io/spring-statemachine
+
+http://hekailiang.github.io/squirrel
--- src/main/java/com/cxd/spring/Application.java
+++ src/main/java/com/cxd/spring/Application.java
@@ -2,22 +2,23 @@ package com.cxd.spring;
 
 import com.cxd.event.Events;
 import com.cxd.state.States;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.statemachine.StateMachine;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-    @Autowired
+    @Resource
     private StateMachine<States, Events> stateMachine;
 
     @Override
     public void run(String... args) throws Exception {
-        stateMachine.sendEvent(Events.E1);
-        stateMachine.sendEvent(Events.E2);
+        stateMachine.sendEvent(Events.CallDialed);
+        stateMachine.sendEvent(Events.CallConnected);
     }
 
     public static void main(String[] args) {
